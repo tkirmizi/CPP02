489fffe647f6e54d1932d353b2c943a3006ee438
tkirmizi <t.kirmizioglu@outlook.com>
2025-02-06 16:24:35 +0100

    class skeleton made

---
+++ ex00/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkirmizi <tkirmizi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 15:56:52 by tkirmizi          #+#    #+#             */
+/*   Updated: 2025/02/06 15:57:51 by tkirmizi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+/*
+orthodox canonical class
+1. default const
+2. copy cost
+3. copy assing opr
+4. dest
+*/

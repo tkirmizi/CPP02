cf090840db4d91afa3063db396ac7d86580b5a59
tkirmizi <t.kirmizioglu@outlook.com>
2025-02-07 11:01:41 +0100

    base stucture copied from ex00 to ex01, def constructors and member functions made

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taha <taha@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 10:12:21 by taha              #+#    #+#             */
+/*   Updated: 2025/02/07 11:00:54 by taha             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+// Constructors
+Fixed::Fixed(): _nbr(0){std::cout << "Default constructor called" << std::endl;}
+
+Fixed::Fixed(const int n): _nbr(n << _frbits)
+{std::cout << "Int constructor called" << std::endl;}
+
+Fixed::Fixed (const float n): _nbr(std::roundf (n * (1 << _frbits)))
+{std::cout << "Int constructor called" << std::endl;}
+
+// Copy constructor
+Fixed::Fixed(const Fixed& other){
+	std::cout << "Copy constructor called" << std::endl; 
+	*this = other;
+}
+
+// Copy assgn operator
+Fixed& Fixed::operator=(const Fixed& other){
+	std::cout << "Copy assignment operator called" << std::endl; 
+	if (this != &other)
+		this->_nbr = other._nbr;
+	return *this;
+}
+
+// Destructor
+Fixed::~Fixed()
+{std::cout << "Destructor called" << std::endl;}
+
+// Member functions
+int Fixed::getRawbits(void) const{
+	std::cout << "getRawbits member function called" << std::endl;
+	return _nbr;
+}
+
+void Fixed::setRawbits(int const raw){
+	_nbr = raw;
+}
+
+int Fixed::toInt( void ) const {
+	return _nbr >> _frbits;
+}
+
+float Fixed::toFloat( void ) const {
+	return (float)_nbr / 256;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: taha <taha@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 10:12:13 by taha              #+#    #+#             */
+/*   Updated: 2025/02/07 10:56:35 by taha             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <string>
+#include <iostream>
+
+
+class Fixed{
+	private:
+		int _nbr;
+		static const int _frbits = 8;
+	public:
+		Fixed(); // def constr
+		Fixed(const int n);
+		Fixed (const float n);
+		Fixed(const Fixed& other); // copy constr
+		Fixed& operator=(const Fixed& other); // copy assgn opr
+		~Fixed(); // destructor
+		int getRawbits(void) const;
+		void setRawbits(int const raw);
+		float toFloat( void ) const;
+		int toInt( void ) const;
+};
+
+#endif
